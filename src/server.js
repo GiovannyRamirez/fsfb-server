04ae044f5ab6ac04a4f3d417f8fb5b0a39328c98
GiovannyRamirez <giovannyramirezs@hotmail.com>
2024-02-29 11:12:22 -0500

    Add products server connection

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("FSFB Server running");
+const dotenv = require("dotenv");
+const express = require("express");
+
+const { errorHandlerMiddleware } = require("./middlewares/errorHandler");
+
+const { ENDPOINTS } = require("./constants/endpoints");
+
+const productsRouter = require("./routes/products");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+
+app.use(ENDPOINTS.PRODUCTS.BASE, productsRouter);
+
+app.use(errorHandlerMiddleware);
+
+app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
